9b970dd8dc6d1f371dc1aaf36c5f35d7cf46f8bb
Anastasiya-Cher <avdukova2904@gmai.com>
2025-05-24 17:16:46 +0300

    final

--- src/script.js
+++ src/script.js
@@ -85,15 +85,54 @@
 const modalWindow = document.getElementById("modal-window");
 const modalWindowOpen = document.getElementById("open-window");
 const modalClose = document.getElementById("modal-btn-cls");
-
+const modalWindowC = document.getElementById("modal-content");
+const modalWindowB = document.getElementById("modal-bl");
 
 modalWindowOpen.addEventListener('click', () => {
   modalWindow.style.display = 'block';
-  
+  modalWindowC.style.display = 'block';
+  modalWindowB.style.display = 'block';
 });
 
 modalClose.addEventListener('click', () => {
   modalWindow.style.display = 'none';
+  modalWindowC.style.display = 'none';
+  modalWindowB.style.display = 'none';
   
 });
 
+
+const modalMenuOpen = document.getElementById ("menu-open")
+const modalMenu = document.getElementById ("hidden-menu")
+const modalMenuClose = document.getElementById ("bok-menu__esc")
+const modalMenuCon = document.getElementById ("bok-menu")
+const gps = document.getElementById ("menu__gps")
+const esc = document.getElementById ("bok-menu__esc")
+const srcM = document.getElementById ("bok-menu__src")
+const footer = document.getElementById ("footer")
+const footerCont = document.getElementById ("footer__contact")
+
+modalMenuOpen.addEventListener('click', () => {
+  modalMenu.style.display = 'block';
+  modalMenuCon.style.display = 'block';
+  modalMenuClose.style.display = 'block';
+  footer.style.left = '5px';
+  footer.style.position = 'fixed';
+  footer.style.bottom = '0px';
+  footer.style.zIndex = '300';
+  srcM.style.display = 'block';
+  footerCont.style.display = 'block';
+  gps.style.position = 'fixed';
+});
+
+modalMenuClose.addEventListener('click', () => {
+  modalMenu.style.display = 'none';
+  modalMenuCon.style.display = 'none';
+  modalMenuClose.style.display = 'none';
+  footer.style.left = '0';
+  footer.style.position = '';
+  footer.style.bottom = '';
+  srcM.style.display = 'none';
+  footerCont.style.display = 'none';
+  gps.style.position = 'static';
+});
